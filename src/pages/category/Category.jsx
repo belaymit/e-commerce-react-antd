a4a6518054ff1f4edb5bfab8cf4fbcd70bbeecc1
belaymit <2belamit@gmail.com>
2023-02-19 19:30:06 +0300

    added Category component

---
+++ src/pages/category/Category.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Products from '../../components/products/Products';
+
+const Category = () => (
+  <div className="productContainer"><Products /></div>
+);
+
+export default Category;
